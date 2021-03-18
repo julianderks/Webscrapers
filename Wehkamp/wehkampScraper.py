5153641502efcf4a3d562c77528f81b48950c228
|-_'JulianD'_-| <49167464+julianderks@users.noreply.github.com>
2021-03-18 19:29:46 +0100

    Update wehkampScraper.py

--- Wehkamp/wehkampScraper.py
+++ Wehkamp/wehkampScraper.py
@@ -10,7 +10,7 @@ base_url = 'https://www.wehkamp.nl/'
 headers = {
 'accept': '*/*',
 'Referer': 'https://www.wehkamp.nl/kleding/',
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+'User-Agent': 'XXXXXXX',
 'content-type': 'application/json',}
 
 # get
